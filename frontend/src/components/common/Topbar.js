8dc23b4769481fedf57cac4ae1418a1e6b294d35
Sadan Arshad <sadanarshad@gmail.com>
2017-11-17 09:04:33 +0500

    home componenet refactoring - included routes - organizaed code in separate components - added react-router and react-router-dom package

---
+++ frontend/src/components/common/Topbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+class Topbar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Readable</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Topbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
@@ -9,7 +10,9 @@ import './assets/css/index.css';
 import './assets/css/App.css';
 
 ReactDOM.render(
-  <App />, 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 registerServiceWorker();
