62e1e7b49f5ce11fe401865fe277c21523711a4d
Sadan Arshad <sadanarshad@gmail.com>
2017-11-20 02:03:41 +0500

    posts sorting

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -9,8 +9,13 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      category: null
+      category: null,
+      sort: true  // if true sort by date else sort by score
     };
+
+    this.sortHandler = this.sortHandler.bind(this);
+    this.sortByDate = this.sortByDate.bind(this);
+    this.sortByScore = this.sortByScore.bind(this);
   }
 
   componentDidMount() {
@@ -37,58 +42,70 @@ class Home extends Component {
     return new Date(timestamp).toLocaleDateString("en-US", options);
   }
 
+  sortHandler(key) {
+    console.log(key)
+    let sort = key === 'date' ? true : false;
+    this.setState({ sort });
+  }
+  
+  sortByDate(a, b) {
+    return (new Date(b.timestamp) - new Date(a.timestamp));
+  }
+
+  sortByScore(a, b) {
+    return b.voteScore - a.voteScore;
+  }
+
   render() {
-    let { category } = this.state;
+    let { category, sort } = this.state;
     let posts = !category ? this.props.posts : this.props.posts.filter((post) => post.category === category);
 
+    sort ? posts.sort(this.sortByDate) : posts.sort(this.sortByScore)
     return (
-      <div>
-        <Row>
-          <Col md={8}>
-            <div style={{marginBottom: '20px'}}>
-              <Nav bsStyle="tabs">
-                <NavItem active>All</NavItem>
-                <NavItem>Newest</NavItem>
-                <NavItem>Top</NavItem>
-              </Nav>
-            </div>
-            <div>
-              {posts && posts.length
-                ? posts.map((post) => (
-                  <Panel key={post.id}
-                    footer={
-                      <div>
-                        <span>Votes: {post.voteScore}</span>
-                        <div style={{float: 'right', color: '#999999'}}>
-                          <span>
-                            <strong>created at</strong> {this.convertDate(post.timestamp)} <strong>by</strong> {post.author}
-                          </span>
-                        </div>
+      <Row>
+        <Col md={8}>
+          <div style={{marginBottom: '20px'}}>
+            <Nav bsStyle="tabs" onSelect={this.sortHandler}>
+              <NavItem active={sort} eventKey='date'>All</NavItem>
+              <NavItem active={!sort} eventKey='score'>Top scorer</NavItem>
+            </Nav>
+          </div>
+          <div>
+            {posts && posts.length
+              ? posts.sort().map((post) => (
+                <Panel key={post.id}
+                  footer={
+                    <div>
+                      <span>Votes: {post.voteScore}</span>
+                      <div style={{float: 'right', color: '#999999'}}>
+                        <span>
+                          <strong>created at</strong> {this.convertDate(post.timestamp)} <strong>by</strong> {post.author}
+                        </span>
                       </div>
-                    }>
-                      <p style={{fontSize: '16px'}}>{post.title}</p>
-                    <span style={{
-                      padding: '3px 5px', 
-                      backgroundColor: '#5fba7d',
-                      display: 'table',
-                      color: '#fff'
-                    }}>
-                      {post.category}
-                    </span>
-                  </Panel>
-                ))
-                : null
-              }
-            </div>
-          </Col>
-          <Col md={4}>
-            <Button bsSize="large" bsStyle="success" block style={{ marginBottom: '20px'}}>
-              Add Post
-            </Button>
-            <CategoriesList />
-          </Col>
-        </Row>
-      </div>
+                    </div>
+                  }>
+                    <p style={{fontSize: '16px'}}>{post.title}</p>
+                  <span style={{
+                    padding: '3px 5px', 
+                    backgroundColor: '#5fba7d',
+                    display: 'table',
+                    color: '#fff'
+                  }}>
+                    {post.category}
+                  </span>
+                </Panel>
+              ))
+              : null
+            }
+          </div>
+        </Col>
+        <Col md={4}>
+          <Button bsSize="large" bsStyle="success" block style={{ marginBottom: '20px'}}>
+            Add Post
+          </Button>
+          <CategoriesList />
+        </Col>
+      </Row>
     );
   }
 }
