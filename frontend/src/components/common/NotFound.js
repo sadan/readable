07954b43b8b2043a2b3a666bebf7b13df574bfd0
Sadan Arshad <sadanarshad@gmail.com>
2017-12-05 23:15:41 +0500

    NotFound component

---
+++ frontend/src/components/common/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <p style={{fontSize: '24px', color: 'gray', margin: 'auto'}}>Not Found</p>
+      <p>The page you are trying to access doesn't exist!</p>
+    </div>
+  )
+}
